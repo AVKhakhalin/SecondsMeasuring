e468089f71f014ab536e65c47f88f8a3d1717879
Khakhalin Andrey Vladimirovich <avkhakhalin@mail.ru>
2022-01-30 19:22:19 +0300

    Выполнение 1 пункта ДЗ: Создан макет таймера и очерчены слои приложения.
    Создан макет таймера и созданы packages для классов приложения на основе чистой архитектуры.
    
    Адрес репозитория на Github:
    https://github.com/AVKhakhalin/SecondsMeasuring

--- app/src/main/java/ru/geekbrains/popular/libraries/secondsmeasuring/view/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/popular/libraries/secondsmeasuring/view/MainActivity.kt
@@ -1,9 +1,10 @@
-package ru.geekbrains.popular.libraries.secondsmeasuring
+package ru.geekbrains.popular.libraries.secondsmeasuring.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.geekbrains.popular.libraries.secondsmeasuring.R
 
-class MainActivity : AppCompatActivity() {
+class MainActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
